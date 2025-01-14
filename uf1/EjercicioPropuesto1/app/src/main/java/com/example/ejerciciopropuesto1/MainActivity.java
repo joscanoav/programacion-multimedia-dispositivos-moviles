433fcdff5b7de2a42ad78cd6ebb27a021a3a54fc
joscanoav <joscanoav@gmail.com>
2025-01-14 12:00:00 +0100

    Step Complete 15

--- uf1/EjercicioPropuesto1/app/src/main/java/com/example/ejerciciopropuesto1/MainActivity.java
+++ uf1/EjercicioPropuesto1/app/src/main/java/com/example/ejerciciopropuesto1/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.ejerciciopropuesto1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    protected TextView texto1;
+    protected TextView texto2;
+    protected EditText caja1;
+    protected Button boton1;
+
+    protected String contenidocaja1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +30,30 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        texto1 = findViewById(R.id.texto1_main);
+        texto2 = findViewById(R.id.texto2_main);
+        boton1 = findViewById(R.id.boton1_main);
+        caja1 = findViewById(R.id.caja1_main);
+
+        boton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                contenidocaja1 = caja1.getText().toString().trim(); // Eliminamos espacios en blanco
+
+                if (contenidocaja1.equals("")) {
+                    texto2.setText("Debe rellenar el valor");
+                    Toast.makeText(MainActivity.this, "Debe rellenar la caja de texto", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (contenidocaja1.equalsIgnoreCase("blanco")) {
+                        texto2.setText("Correcto");
+                    } else {
+                        texto2.setText("Error");
+                    }
+                }
+            }
         });
     }
 }
