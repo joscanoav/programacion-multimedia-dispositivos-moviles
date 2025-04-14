12c4e99810a4dee1180b0beaba85a6e2dca1a754
joscanoav <joscanoav@gmail.com>
2025-04-14 11:32:55 +0200

    Exercise proposed 2 completed

--- uf1/ListadoJugadoresDosPantallas/app/src/main/java/com/example/listadojugadoresdospantallas/MainActivity.java
+++ uf1/ListadoJugadoresDosPantallas/app/src/main/java/com/example/listadojugadoresdospantallas/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.listadojugadoresdospantallas;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +14,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    protected ListView lista1;
+
+    protected ArrayList<String> listajugadores = new ArrayList<String>();
+    protected ArrayAdapter<String> adaptador;
+
+    protected String contenidoItem="";
+
+    protected Intent pasarPantalla;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +39,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        lista1 = (ListView) findViewById(R.id.lista1_main);
+
+        listajugadores.add("Curry");
+        listajugadores.add("Jordan");
+        listajugadores.add("Lebron");
+
+        adaptador = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,listajugadores);
+        lista1.setAdapter(adaptador);
+
+        lista1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                contenidoItem = parent.getItemAtPosition(position).toString();
+                Toast.makeText(MainActivity.this, "Item: " + contenidoItem, Toast.LENGTH_SHORT).show();
+                pasarPantalla = new Intent(MainActivity.this, SegundaActividad.class);
+                pasarPantalla.putExtra("PAQUETE", contenidoItem);
+                startActivity(pasarPantalla);
+            }
+        });
+
+
     }
 }
--- uf1/ListadoJugadoresDosPantallas/app/src/main/java/com/example/listadojugadoresdospantallas/SegundaActividad.java
+++ uf1/ListadoJugadoresDosPantallas/app/src/main/java/com/example/listadojugadoresdospantallas/SegundaActividad.java
@@ -1,6 +1,11 @@
 package com.example.listadojugadoresdospantallas;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,16 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SegundaActividad extends AppCompatActivity {
+
+    protected ImageView ima1;
+    protected Button boton1;
+    
+    protected String paquete1;
+    
+    protected Bundle extras;
+
+    protected Intent pasarPantalla;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +35,38 @@ public class SegundaActividad extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ima1=(ImageView) findViewById(R.id.ima1_segunda);
+        boton1=(Button) findViewById(R.id.boton1_segunda);
+
+        boton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pasarPantalla = new Intent(SegundaActividad.this, MainActivity.class);
+                startActivity(pasarPantalla);
+            }
+        });
+        
+        //recojo todos los paquetes
+        
+        extras= getIntent().getExtras();
+        
+        if (extras!=null)// he recogido al menos un paquete
+        {
+            paquete1= extras.getString("PAQUETE");
+            if (paquete1.equalsIgnoreCase("Lebron")){
+                ima1.setImageResource(R.drawable.lebron);
+            }
+            else if (paquete1.equalsIgnoreCase("Curry")){
+                ima1.setImageResource(R.drawable.curry);
+            }
+            if (paquete1.equalsIgnoreCase("Jordan")){
+                ima1.setImageResource(R.drawable.jordan);
+            }
+        }
+        else{
+            Toast.makeText(this, "Sin paquete", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
