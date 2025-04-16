ca08f43dfac0c8d88d1b11ae67b9f1bad567b8d5
joscanoav <joscanoav@gmail.com>
2025-04-16 10:48:31 +0200

    Set up menu options

--- uf1/MenuOrientacion/app/src/main/java/com/example/menuorientacion/MainActivity.java
+++ uf1/MenuOrientacion/app/src/main/java/com/example/menuorientacion/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.menuorientacion;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
@@ -8,8 +9,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    protected ImageView ima1;
+    protected TextView texto1;
+
+    protected Intent pasarpantalla;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +36,45 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        ima1 = (ImageView) findViewById(R.id.ima1_main);
+        texto1 = (TextView) findViewById(R.id.texto1_main);
 
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.menu_main, menu);
+            return true;
+        }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+        if (id== R.id.menu_item_borrar)
+        {
+            Toast.makeText(this, "He pulsado en borrar", Toast.LENGTH_SHORT).show();
+            texto1.setText("");
+            return true;
+        }
+        else if (id==R.id.menu_item_reiniciar)
+        {
+            Toast.makeText(this, "He pulsado reiniciar", Toast.LENGTH_SHORT).show();
+            pasarpantalla = new Intent(MainActivity.this, MainActivity.class);
+            startActivity(pasarpantalla);
+
+            return true;
+        }
+        else
+        {
+            return super.onOptionsItemSelected(item);
+        }
+        
+        // Handle item selection.
 
     }
-}
+
+    }
+
+
+
