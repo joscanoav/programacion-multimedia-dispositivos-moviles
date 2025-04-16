2a2f93c9e3be58bf143efda15e3b83c1e2b87fdf
joscanoav <joscanoav@gmail.com>
2025-04-17 01:26:13 +0200

    Complete GeneroActivity

--- uf1/FormularioCompleto/app/src/main/java/com/example/formulariocompleto/GeneroActivity.java
+++ uf1/FormularioCompleto/app/src/main/java/com/example/formulariocompleto/GeneroActivity.java
@@ -3,6 +3,7 @@ package com.example.formulariocompleto;
 import android.os.Bundle;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +16,12 @@ public class GeneroActivity extends AppCompatActivity {
     protected TextView texto1;
     protected ImageButton imaboton1;
     protected ImageButton imaboton2;
+
+    protected String paquete1="";
+    protected String paquete2="";
+
+    protected Bundle extras;
+
 
 
     @Override
@@ -31,5 +38,16 @@ public class GeneroActivity extends AppCompatActivity {
         texto1=(TextView) findViewById(R.id.texto1_genero);
         imaboton1=(ImageButton) findViewById(R.id.imaboton1_genero);
         imaboton2=(ImageButton) findViewById(R.id.imaboton2_genero);
+
+        extras=getIntent().getExtras();
+        if (extras!=null) // no ha llegado algun paquete
+        {
+            paquete1=extras.getString("NOMBRE");
+            paquete2=extras.getString("APELLIDOS");
+
+            Toast.makeText(this, paquete1 +"- "+paquete2, Toast.LENGTH_SHORT).show();
+
+
+        }
     }
 }
