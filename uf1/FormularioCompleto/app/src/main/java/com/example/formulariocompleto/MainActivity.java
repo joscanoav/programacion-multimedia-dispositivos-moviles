ffd597fa75eb7d2dee7aabea5ca9fa8d18dddf09
joscanoav <joscanoav@gmail.com>
2025-04-17 01:12:02 +0200

    Add splash to MainActivity

--- uf1/FormularioCompleto/app/src/main/java/com/example/formulariocompleto/MainActivity.java
+++ uf1/FormularioCompleto/app/src/main/java/com/example/formulariocompleto/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.formulariocompleto;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -10,10 +11,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
 
     protected ImageView ima1;
     protected TextView  texto1;
+
+    protected TimerTask tt;
+    protected Timer t;
+
+    protected Intent pasarpantalla;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +37,18 @@ public class MainActivity extends AppCompatActivity {
 
         ima1=(ImageView) findViewById(R.id.ima1_main);
         texto1 =(TextView) findViewById(R.id.texto1_main);
+
+        tt=new TimerTask() {
+            @Override
+            public void run() {
+                pasarpantalla= new Intent(MainActivity.this,InformacionActivity.class);
+                startActivity(pasarpantalla);
+
+            }
+        };
+
+        t=new Timer();
+        t.schedule(tt,3000);
+
     }
 }
