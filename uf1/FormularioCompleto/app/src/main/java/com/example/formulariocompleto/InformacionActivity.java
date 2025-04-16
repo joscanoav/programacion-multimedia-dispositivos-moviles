64172a439947daecf51634c7220fc34fde8b4418
joscanoav <joscanoav@gmail.com>
2025-04-17 01:20:16 +0200

    Set up Information Activity

--- uf1/FormularioCompleto/app/src/main/java/com/example/formulariocompleto/InformacionActivity.java
+++ uf1/FormularioCompleto/app/src/main/java/com/example/formulariocompleto/InformacionActivity.java
@@ -1,9 +1,12 @@
 package com.example.formulariocompleto;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +23,12 @@ public class InformacionActivity extends AppCompatActivity {
     protected EditText caja2;
 
     protected Button boton1;
+    
+    protected String contenidoCaja1="";
+    protected String contenidoCaja2="";
+
+    protected Intent pasarpantalla;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,5 +46,36 @@ public class InformacionActivity extends AppCompatActivity {
         caja1=(EditText) findViewById(R.id.caja1_info);
         caja2=(EditText) findViewById(R.id.caja2_info);
         boton1=(Button) findViewById(R.id.boton1_info);
+        
+        
+        boton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+                contenidoCaja1= caja1.getText().toString();
+                contenidoCaja2=caja2.getText().toString();
+                
+                if (!contenidoCaja1.equalsIgnoreCase("")&& !contenidoCaja2.equalsIgnoreCase(""))
+                {
+                    // envia la informacion
+                    pasarpantalla= new Intent(InformacionActivity.this,GeneroActivity.class);
+                    pasarpantalla.putExtra("NOMBRE",contenidoCaja1);
+                    pasarpantalla.putExtra("APELLIDOS",contenidoCaja2);
+                    startActivity(pasarpantalla);
+
+
+                }
+                else
+                {
+                    Toast.makeText(InformacionActivity.this, "Debe rellenar los campos de texto", Toast.LENGTH_SHORT).show();
+                }
+                
+            }
+        });
+        
+        
+        
+        
+        
     }
 }
