33bb6a35cbe88087cf263f07bd3120d594a329b0
joscanoav <joscanoav@gmail.com>
2025-04-26 09:38:43 +0200

    Set up ReproductorOnlineActivity

--- uf1/ReproductorAudio/app/src/main/java/com/example/reproductoraudio/ReproductorOnlineActivity.java
+++ uf1/ReproductorAudio/app/src/main/java/com/example/reproductoraudio/ReproductorOnlineActivity.java
@@ -1,6 +1,10 @@
 package com.example.reproductoraudio;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ReproductorOnlineActivity extends AppCompatActivity {
+
+    protected TextView texto1;
+    protected ImageButton imabotonPlay;
+    protected ImageButton imabotonPause;
+
+    protected ImageButton imabotonStop;
+
+    protected MediaPlayer mp;
+    protected float milisegundo=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +32,46 @@ public class ReproductorOnlineActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        texto1=(TextView) findViewById(R.id.text1_online);
+        imabotonPlay=(ImageButton) findViewById(R.id.imabotonPlay_online;
+        imabotonPause=(ImageButton) findViewById(R.id.imabotonPause_online);
+        imabotonStop=(ImageButton) findViewById(R.id.imabotonStop_online);
+
+        imabotonPause.setVisibility(View.GONE);
+        imabotonStop.setVisibility(View.GONE);
+
+
+        imabotonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //play
+
+
+            }
+        });
+
+        imabotonPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //pause
+
+
+
+
+            }
+        });
+
+        imabotonStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //stop
+
+
+
+            }
         });
     }
 }
