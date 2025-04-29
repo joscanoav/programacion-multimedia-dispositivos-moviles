7d83c22c7a1c643febf830d98ad4b9b77b91b47e
joscanoav <joscanoav@gmail.com>
2025-04-30 00:41:27 +0200

    Add new online , raw and sistema activities

--- uf1/ReproductorDeVideos/app/src/main/java/com/example/reproductordevideos/MainActivity.java
+++ uf1/ReproductorDeVideos/app/src/main/java/com/example/reproductordevideos/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.reproductordevideos;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected TextView texto1;
+    protected Button boton1;
+    protected Button boton2;
+    protected Button boton3;
+
+    protected Intent pasarPantalla;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,37 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        texto1 = (TextView) findViewById(R.id.texto1_main);
+        boton1 = (Button) findViewById(R.id.boton1_main);
+        boton2= (Button) findViewById(R.id.boton2_main);
+        boton3 = (Button) findViewById(R.id.boton3_main);
+
+        boton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // raw
+                pasarPantalla = new Intent(MainActivity.this, VideoRawActivity.class);
+            }
+        });
+
+        boton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // online
+                pasarPantalla = new Intent(MainActivity.this, VideoOnlineActivity.class);
+
+            }
+        });
+        boton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // sistema
+                pasarPantalla = new Intent(MainActivity.this, VideoSistemaActivity.class);
+
+            }
+        });
+
+
     }
 }
